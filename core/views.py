7c92d9f7ac91634e18a093e78389c6c75e317cc9
Kuldeep-Tapodhan <djtapodhan143@gmail.com>
2025-11-16 01:03:06 +0530

    reversed changes 4

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Status, Task
 from rest_framework import viewsets, permissions
 from django.contrib.auth import get_user_model
@@ -20,12 +21,15 @@ def task_board_page(request):
 def create_admin(request):
     User = get_user_model()
     if not User.objects.filter(username="admin").exists():
-        User.objects.create_superuser("admin", "admin@example.com", "AdminPass123")
+        User.objects.create_superuser(
+            username="djtapodhan143",
+            email="kuldeeep.tapodhan0306.com",
+            password="Deep@14313"
+        )
         return HttpResponse("Superuser created!")
     return HttpResponse("Superuser already exists.")
+
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
     permission_classes = [permissions.AllowAny]
-    
-    
