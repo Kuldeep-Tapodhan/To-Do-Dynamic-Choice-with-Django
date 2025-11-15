dc3869c48da50925e3af56feac5e12d27f861141
Kuldeep-Tapodhan <djtapodhan143@gmail.com>
2025-11-15 23:25:10 +0530

    fixed first issue

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from django.core.management import call_command
 
 def main():
     """Run administrative tasks."""
@@ -15,6 +15,16 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+        
+if os.environ.get("AUTO_CREATE_SUPERUSER") == "true":
+    try:
+        call_command("createsuperuser",
+                     username=os.environ["DJANGO_SUPERUSER_USERNAME"],
+                     email=os.environ["DJANGO_SUPERUSER_EMAIL"],
+                     interactive=False)
+    except:
+        pass
+
     execute_from_command_line(sys.argv)
 
 
